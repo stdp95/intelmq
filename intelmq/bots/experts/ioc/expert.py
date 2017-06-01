791af2e1e8619edde76d53363b42bfcbbf2a41dd
navtej <n.s.buttar+github@gmail.com>
2017-06-01 13:39:25 +0530

    check for existing extra

--- intelmq/bots/experts/ioc/expert.py
+++ intelmq/bots/experts/ioc/expert.py
@@ -5,18 +5,27 @@ IOC expert bot adds ioc array in extra.ioc_type
 
 from intelmq.lib.bot import Bot
 
+
 class IOCExpertBot(Bot):
 
     def process(self):
         ioc_types = set()
         event = self.receive_message()
         for key in event:
-            if key.endswith('.ip'): ioc_types.add('ip')
-            if key.endswith('.fqdn'): ioc_types.add('domain')
-            if key.endswith('.url'): ioc_types.add('url')
-            if key.startswith('malware.hash'): ioc_types.add('hash')
-
-        event.add('extra',{'ioc_types': list(ioc_types)})
+            if key.endswith('.ip'):
+                ioc_types.add('ip')
+            if key.endswith('.fqdn'):
+                ioc_types.add('domain')
+            if key.endswith('.url'):
+                ioc_types.add('url')
+            if key.startswith('malware.hash'):
+                ioc_types.add('hash')
+        if 'extra' in event:
+            extra = event['extra']
+            extra['ioc_types'] = list(ioc_types)
+            event.change('extra', extra)
+        else:
+            event.add('extra', {'ioc_types': list(ioc_types)})
         self.send_message(event)
         self.acknowledge_message()
 
